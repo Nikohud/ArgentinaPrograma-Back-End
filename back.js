73e0d56951e61e1dc2441f30db56c6f6bd68a9d8
nikohud <nikohud86@gmail.com>
2022-10-24 12:52:12 -0300

    secondgit

---
+++ back.js
@@ -0,0 +1,8 @@
+// Primer array solo para probar github
+
+let arrObj = [
+    {user: "NikoHud", Age: "25", Isdeveloper: true},
+    {user: "Floor", Age: "25", Isdeveloper: false},
+    {user: "erne", Age: "24", Isdeveloper: true},
+    {user: "Pachi", Age: "28", Isdeveloper: false}
+]
